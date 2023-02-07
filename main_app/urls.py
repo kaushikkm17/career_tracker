151cc5bf929047a26bfefcf2f1ca9830703eadb8
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2023-02-07 16:04:51 -0500

    add show routes and functions

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('todo/', views.todo, name='todo'),
+    path('connections/', views.connections_index, name='connections_index'),
+    path('connections/<int:connection_id>', views.connections_detail, name='connections_detail'),
+    path('events/', views.events, name='events'),
+    path('skills/', views.skills, name='skills'),
+    path('search_parameters/', views.search_parameters, name='search_parameters'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def todo(request):
+    return render(request, 'todo.html')
+    
+def connections_index(request):
+    return render(request, 'connections/index.html')
+
+def connections_detail(request):
+    return render(request, 'connections/detail.html')
+
+def events(request):
+    return render(request, 'events.html')
+
+def skills(request):
+    return render(request, 'skills.html')
+
+def search_parameters(request):
+    return render(request, 'search_parameters.html')
+
+
